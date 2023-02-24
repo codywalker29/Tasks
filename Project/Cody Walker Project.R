31a78f5563e92d8bcc6665639c13e6ddb462fb9f
codywalker24 <cjw0070@mix.wvu.edu>
2023-02-23 20:48:59 -0500

    Cody Walker Project

---
+++ Project/Cody Walker Project.R
@@ -0,0 +1,3 @@
+setwd('C:\\Users\\Jimmy\\Desktop\\Evolution\\Tasks\\Project')
+Plantdata<-read.csv("C:\\Users\\Jimmy\\Desktop\\Evolution\\Tasks\\Project\\Cody Walker Plant Offspring Data.csv")
+Parentaldata<-read.csv("C:\\Users\\Jimmy\\Desktop\\Evolution\\Tasks\\Project\\Upload #1 FP Cody Walker.csv")
