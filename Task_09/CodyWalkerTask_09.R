dca041910d648ca39614626d5067d0e8ce27d39c
codywalker24 <cjw0070@mix.wvu.edu>
2023-04-21 10:51:16 -0400

    Cody Walker Task 9

---
+++ Task_09/CodyWalkerTask_09.R
@@ -0,0 +1,16 @@
+setwd('C:\\Users\\Jimmy\\Desktop\\Evolution\\Tasks\\Task_09')
+library(diversitree)
+transition_0to1<-0.1
+transition_1to0<-0.1
+speciation_0<-0.2
+extinction_0<-0.1
+speciation_1<-0.2
+extinction_1<-0.1
+maxN<-1e3
+maxT<-50
+Pars<-c(speciation_0,speciation_1,extinction_0,extinction_1,transition_0to1,transition_1to0)
+simTree<-tree.bisse(Pars,max.taxa=maxN,max.t=maxT)
+str(simTree)
+?tree.bisse
+stateTable<-table(simTree$tip.state)
+stateTable/sum(stateTable)
